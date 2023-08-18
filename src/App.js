3af119199470364ba506390c4bd4f5e832351ee7
rotembab <rotembabmen@gmail.com>
2023-08-18 08:16:20 +0300

    added TODO Note in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="">
+      {/* TODO: Make here the router so there only has to be one Navbar Code comp */}
       <Navbar />
     </div>
   );
