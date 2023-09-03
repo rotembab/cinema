4b7b7ca407862c26d6ff12880e4ce9cb7b7f112a
rotembab <rotembabmen@gmail.com>
2023-09-03 20:09:24 +0300

    Added navigation

--- src/Components/CategoryItem.js
+++ src/Components/CategoryItem.js
@@ -2,16 +2,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function CategoryItem({ href, src, alt, name }) {
-  const NavToLink = () => {
-    useNavigate(href);
-  };
+  const navigate = useNavigate();
   return (
     <div
       className="w-[163px] h-[275px] hover:cursor-pointer"
-      onClick={NavToLink}>
+      onClick={() => {
+        navigate(href);
+      }}>
       <img className="w-[163px] h-[242px]" src={src} alt={alt} />
       <p className="hover:underline-orange ">{name}</p>
-      <a href={href}></a>
     </div>
   );
 }
