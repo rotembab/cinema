f12cd715f4ba9827866197a7732fc1b60be437dc
rotembab <rotembabmen@gmail.com>
2023-09-03 20:21:28 +0300

    Added fetch for movie page

--- src/Components/Pages/MoviePage.js
+++ src/Components/Pages/MoviePage.js
@@ -1,5 +1,31 @@
 import React from "react";
+import Navbar from "../Navbar";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Apis from "../../Literals/Apis";
 
 export default function MoviePage() {
-  return <div>MoviePage</div>;
+  const [movieData, setMovieData] = useState({});
+
+  const params = useParams();
+  const fetchMovie = async () => {
+    try {
+      const resp = await axios.get(`${Apis.Movies}/${params.id}`);
+      setMovieData(resp.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovie();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <h1>{movieData.name}</h1>
+    </div>
+  );
 }
